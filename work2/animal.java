88a1edce8cd1d3e243487a6082c0a0a2b7e3acd2
lixiangyu-1998 <79912688+lixiangyu-1998@users.noreply.github.com>
2021-03-24 17:07:19 +0800

    Add files via upload

---
+++ work2/animal.java
@@ -0,0 +1,17 @@
+package work2;
+
+/**
+ * @author:LXY
+ * @className:animal
+ * @description:
+ * @date:2021/3/2021:12
+ * @version:0.1
+ * @since:1.8
+ */
+public class animal {
+    String name;
+    int age;
+    public void move(){
+        System.out.println("我可以移动");
+    }
+}
